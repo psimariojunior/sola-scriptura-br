75b6a1e2a95f00a629da9e5602c66e4c45caccfd
Michele <michele@sola-scriptura.com>
2026-06-18 15:48:28 -0300

    fix: correct dist path in serverless handler

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,7 +2,7 @@ module.exports = async (req, res) => {
   try {
     const serverless = require('serverless-http');
     const { NestFactory } = require('@nestjs/core');
-    const { AppModule } = require('./dist/app.module');
+    const { AppModule } = require('../dist/app.module');
     const { ValidationPipe } = require('@nestjs/common');
 
     const app = await NestFactory.create(AppModule, { logger: ['error', 'warn', 'log'] });

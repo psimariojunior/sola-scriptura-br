07e611860ea54cfb0efc9b571fea3f8c165bba19
Michele <psi_mariojunior@hotmail.com>
2026-07-27 19:35:04 -0300

    fix: incrementar versao do cache SW para forcar atualizacao

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v7';
+const CACHE_VERSION = 'v8';
 const STATIC_CACHE = `ssb-static-${CACHE_VERSION}`;
 const API_CACHE = `ssb-api-${CACHE_VERSION}`;
 const BIBLE_CACHE = `ssb-bible-${CACHE_VERSION}`;

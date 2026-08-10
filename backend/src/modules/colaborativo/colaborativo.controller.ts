35820691afe1d163e5fbce9b95404be5875f4f23
Michele <psi_mariojunior@hotmail.com>
2026-08-10 16:36:25 -0300

    fix: sala colaborativa - rotas publicas + nginx websocket
    - Adicionado @Publico() em todas as 7 rotas do controller colaborativo
    - Adicionado location /socket.io/ no nginx.conf para WebSocket
    - Corrigido timeout de 300s para 86400s (24h) para conexoes persistentes
    - Build OK

--- backend/src/modules/colaborativo/colaborativo.controller.ts
+++ backend/src/modules/colaborativo/colaborativo.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Post, Param, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { Publico } from '../../common/decorators/publico.decorator';
 import { ColaborativoService } from './colaborativo.service';
 import { StudyRoom, RoomMessage, RoomParticipant } from './entities/study-room.entity';
 
@@ -6,17 +7,20 @@ import { StudyRoom, RoomMessage, RoomParticipant } from './entities/study-room.e
 export class ColaborativoController {
   constructor(private readonly service: ColaborativoService) {}
 
+  @Publico()
   @Post('rooms')
   @HttpCode(HttpStatus.CREATED)
   async createRoom(@Body() body: { code: string; name: string; hostUserId: string }): Promise<StudyRoom> {
     return this.service.createRoom(body.code, body.name, body.hostUserId);
   }
 
+  @Publico()
   @Get('rooms/:code')
   async getRoom(@Param('code') code: string): Promise<StudyRoom | null> {
     return this.service.findByCode(code);
   }
 
+  @Publico()
   @Post('rooms/:code/participants')
   async addParticipant(
     @Param('code') code: string,
@@ -25,6 +29,7 @@ export class ColaborativoController {
     return this.service.addParticipant(code, body);
   }
 
+  @Publico()
   @Post('rooms/:code/participants/:participantId/leave')
   async removeParticipant(
     @Param('code') code: string,
@@ -33,6 +38,7 @@ export class ColaborativoController {
     return this.service.removeParticipant(code, participantId);
   }
 
+  @Publico()
   @Post('rooms/:code/messages')
   async addMessage(
     @Param('code') code: string,
@@ -41,6 +47,7 @@ export class ColaborativoController {
     return this.service.addMessage(code, body);
   }
 
+  @Publico()
   @Post('rooms/:code/notes')
   async updateNotes(
     @Param('code') code: string,
@@ -49,6 +56,7 @@ export class ColaborativoController {
     return this.service.updateSharedNotes(code, body.notes);
   }
 
+  @Publico()
   @Post('rooms/:code/deactivate')
   @HttpCode(HttpStatus.OK)
   async deactivateRoom(@Param('code') code: string): Promise<void> {
